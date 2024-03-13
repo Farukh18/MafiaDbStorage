a7f1859eee552f3e000b4dec0e10e09882a007e3
Farukh <excommunicad00@yandex.ru>
2024-03-13 22:15:23 +0600

    first commit

---
+++ src/main/java/com/example/mafiadbstorage/MafiaDbStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mafiadbstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaDbStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaDbStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mafiadbstorage/MafiaDbStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mafiadbstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaDbStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
